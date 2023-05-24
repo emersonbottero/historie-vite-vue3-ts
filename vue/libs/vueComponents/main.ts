9f108e0bd23a06508e42374e81d24e575cd44213
Emerson Bottero <emersonbottero@gmail.com>
2023-05-24 00:52:15 -0300

    building types

--- vue/libs/vueComponents/main.ts
+++ vue/libs/vueComponents/main.ts
@@ -1,4 +1,4 @@
-import { defineCustomElement } from 'vue'
+// import { defineCustomElement } from 'vue'
 import VButton from '../../src/components/SButton.vue'
 
 console.log(VButton.styles) // ["/* inlined css */"]
